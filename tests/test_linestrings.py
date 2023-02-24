4e52cb35ca30dc297a4b4072980c3d899a4c2d50
Balthasar Teuscher <balthasar.teuscher@tum.de>
2023-02-24 12:20:53 +0100

    dry triangulation test

--- tests/test_linestrings.py
+++ tests/test_linestrings.py
@@ -1,20 +1,26 @@
-import h5py;
-from matplotlib import pyplot as plt;
 import sys
-import random;
+from pathlib import Path
+import h5py
+import random
 from tqdm import tqdm
+from matplotlib import pyplot as plt
+
 
 class cfg:
-    N=1000
+    N = 1000
 
-if __name__=="__main__":
-    f = h5py.File(sys.argv[1],"r");
-    f = f["osm"]
+
+if __name__ == "__main__":
+    infile = sys.argv[1] if len(sys.argv) == 2 else "../data/berlin.h5"
+    if not Path(infile).exists():
+        raise RuntimeError(f"H5 file not found {infile}")
+
+    f = h5py.File(infile, "r")["osm"]
     print(f["linestrings_idx"].shape)
-    start = random.randint(0, f["linestrings_idx"].shape[0]-cfg.N-1)
-    linestrings = f["linestrings_idx"][start:(start+cfg.N)][:]
-    for s,e in tqdm(linestrings):
+
+    start = random.randint(0, f["linestrings_idx"].shape[0] - cfg.N - 1)
+    linestrings = f["linestrings_idx"][start : (start + cfg.N)][:]
+    for s, e in tqdm(linestrings):
         coords = f["linestrings"][s:e][:]
-        plt.plot(coords[:,0],coords[:,1])
+        plt.plot(coords[:, 0], coords[:, 1])
     plt.show()
-    
